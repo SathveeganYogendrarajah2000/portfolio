7af523d32cad3554f1aea2048836c453cc283e41
Sathveegan <sathve@gmail.com>
2023-10-07 19:28:51 +0530

    just changed the name

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,7 +4,18 @@ const Home = () => {
   return (
     <div className="homeContainer">
       <div className="homeContainer_hero">
-        <div className="homeContainer_hero_details"></div>
+        <div className="homeContainer_hero_details">
+          <h1 className="homeContainer_hero_details_name">
+            Sathveegan Yogendrarajah
+          </h1>
+          <h2 className="homeContainer_hero_details_degree">
+            Bsc (Hons) in Engineering specialized in Computer Science and
+            Engineering
+          </h2>
+          <div className="homeContainer_hero_details_techStack">
+            TECH STACK <span className="homeContainer_hero_details_techStack_techs">JAVA</span>
+          </div>
+        </div>
         <div className="homeContainer_hero_pic">
           <img src={me} alt="Sathveegan" />
         </div>
